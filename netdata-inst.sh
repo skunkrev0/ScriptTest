509cd47bee1d4236eeb45ea655898d032d7585f7
skunkrev0 <wilfried.pinaud@orange.fr>
2016-05-06 00:40:45 +0200

    Create netdata-inst.sh

---
+++ netdata-inst.sh
@@ -0,0 +1,4 @@
+apt-get install zlib1g-dev gcc make git autoconf autogen automake pkg-config
+git clone https://github.com/firehol/netdata.git --depth=1
+cd netdata ./netdata-installer.sh
+/usr/sbin/netdata
